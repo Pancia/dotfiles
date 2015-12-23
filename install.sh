e1a14d53a91dec8205d1e42916fbf5aabea37068
Anthony D'Ambrosio <adambrosio@thenavisway.com>
2015-12-22 17:52:35 -0800

    maybe fixed vim/ftplugin linking

--- install.sh
+++ install.sh
@@ -3,9 +3,9 @@ cp .bashrc       ~/.bashrc
 cp .vimperatorrc ~/.vimperatorrc
 cp .agignore     ~/.agignore
 
-mkdir -p .vim/ftplugin
+mkdir -p ~/.vim/ftplugin
 for i in $(ls ./.vim/ftplugin); do
-    ln -f ./.vim/ftplugin/$i ~/.vim/ftplugin/$i
+    ln -f vim/ftplugin/$i ~/.vim/ftplugin/$i
 done
 
 exec bash
