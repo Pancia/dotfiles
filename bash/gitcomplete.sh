b9da399830437266ead26c1afa6ccd68a935cf96
Anthony D'Ambrosio <adambrosio@thenavisway.com>
2015-12-22 17:54:09 -0800

    added gundo for undo-ing the last commit

--- bash/gitcomplete.sh
+++ bash/gitcomplete.sh
@@ -35,6 +35,8 @@ __git_complete save _git_stash
 alias gl='git log --graph --all --decorate --abbrev-commit'
 alias gt="git log --graph --abbrev-commit --decorate --date=relative --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all"
 
+alias gundo='git reset --soft HEAD~'
+
 alias gf='git flow'
 alias gff='git flow feature'
 alias gfr='git flow release'
