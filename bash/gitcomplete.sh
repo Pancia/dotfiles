14c996424e7dbcb77dcc13beed0524a15d9c67cd
Anthony D'Ambrosio <adambrosio@thenavisway.com>
2015-12-22 17:54:56 -0800

    added gredo for commiting using the previous commit message after having used gundo

--- bash/gitcomplete.sh
+++ bash/gitcomplete.sh
@@ -36,6 +36,7 @@ alias gl='git log --graph --all --decorate --abbrev-commit'
 alias gt="git log --graph --abbrev-commit --decorate --date=relative --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all"
 
 alias gundo='git reset --soft HEAD~'
+alias gredo='git commit -c ORIG_HEAD'
 
 alias gf='git flow'
 alias gff='git flow feature'
