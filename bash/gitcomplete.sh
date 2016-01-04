bb8b789655f6643918b50d5e3ca1395f4753bce6
Anthony D'Ambrosio <adambrosio@thenavisway.com>
2016-01-04 12:32:18 -0800

    gl now focuses on "HEAD ->" on startup

--- bash/gitcomplete.sh
+++ bash/gitcomplete.sh
@@ -32,7 +32,7 @@ alias stash='git stash'
 __git_complete stash _git_stash
 alias save='git stash save'
 __git_complete save _git_stash
-alias gl='git log --graph --all --decorate --abbrev-commit'
+alias gl='GIT_PAGER="less -p \"\(HEAD -> .*,\"" git log --graph --all --decorate --abbrev-commit'
 alias gt="git log --graph --abbrev-commit --decorate --date=relative --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all"
 
 alias gundo='git reset --soft HEAD~'
