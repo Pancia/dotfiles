cd295c6e5356c8c77ed6302ff08430b4f075ac45
Anthony D'Ambrosio <nsa.anthony@gmail.com>
2017-07-17 15:34:00 -0700

    rm checks files exists before removing, warning if not found

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -1,12 +1,19 @@
 function rm {
-    local dir prefix timestamp
+    local dir prefix timestamp failed
     dir="$(pwd)"
     timestamp="$(date '+%Y-%m-%d_%X')"
     prefix="${dir//\//%}T${timestamp}<->"
-    for i in "$@"; do
-        echo "moved ${i} to" ~/.Trash/${prefix}${i//\//%}
-        mv ${i} ~/.Trash/${prefix}${i//\//%}
+    failed=false
+    for f in "$@"; do
+        if [ -f $f ]; then
+            echo "moved ${f} to" ~/.Trash/${prefix}${f//\//%}
+            mv ${f} ~/.Trash/${prefix}${f//\//%}
+        else
+            failed=true
+            echo "[dotfiles/rm] file not found: $f" >&2
+        fi
     done
+    $failed && exit 1
 }
 
 function _fancy-ctrl-z {
