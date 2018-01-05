37ba20c9885ccd9c5581d828ba41545ec9238caf
Anthony D'Ambrosio <nsa.anthony@gmail.com>
2018-01-05 12:54:45 -0800

    modified disable startup chime to be a /Library/LaunchDaemons/

--- installs/disable_osx_startup_chime/disable_osx_startup_chime.sh
+++ installs/disable_osx_startup_chime/disable_osx_startup_chime.sh
@@ -1,4 +1,5 @@
-PLIST=disable_osx_startup_chime.plist
+PLIST=me.pancia.disable_osx_startup_chime.plist
 
-ln -f $PLIST ~/Library/LaunchAgents/$PLIST
-sudo launchctl load -w ~/Library/LaunchAgents/$PLIST
+sudo launchctl unload -w /Library/LaunchDaemons/$PLIST 2> /dev/null
+sudo ln -f $PLIST /Library/LaunchDaemons/$PLIST
+sudo launchctl load -w /Library/LaunchDaemons/$PLIST
