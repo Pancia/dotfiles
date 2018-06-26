f95b170c7078bc1e2c32477df73de2e6a78a1e39
Anthony D'Ambrosio <anthony.dayzerostudio@gmail.com>
2018-06-26 14:23:19 +0200

    adding global .ignore and .gitignore to tree ignore

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -90,7 +90,13 @@ function gitroot { git rev-parse --show-toplevel "$@" }
 
 function cljs { planck "$@" }
 
-function _gitignore_to_regex { (command cat .gitignore .ignore 2> /dev/null || echo '') | sed 's#^/##' | tr '\n' '|' | sed 's/\|*$//' }
+function _gitignore_to_regex {
+    (command cat .gitignore .ignore ~/.ignore ~/dotfiles/git/gitignore_global 2> /dev/null || echo '') \
+        | sed '/^[#;"]/d' \
+        | sed 's#^/##' \
+        | tr '\n' '|' \
+        | sed 's/\|*$//'
+}
 function tree { command tree -I "$(_gitignore_to_regex)" "$@" }
 
 function ag { command ag --hidden "$@" }
