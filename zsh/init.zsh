57b496866f4499852810f5d1f40286fe31fc0f8b
Anthony D'Ambrosio <anthony.dayzerostudio@gmail.com>
2018-06-27 03:42:05 +0200

    extracted zshrc -> init.zsh

---
+++ zsh/init.zsh
@@ -0,0 +1,10 @@
+source ~/dotfiles/zsh/antigen.zsh
+source ~/dotfiles/zsh/bindings.zsh
+source ~/dotfiles/zsh/config.zsh
+
+source ~/dotfiles/zsh/git-fns.zsh
+
+source ~/dotfiles/zsh/functions.zsh
+source ~/dotfiles/zsh/theme.zsh
+
+[ -e ~/dotfiles/extra.zsh ] && source ~/dotfiles/extra.zsh
