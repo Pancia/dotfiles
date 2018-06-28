dab9b1a81c9935e25fd54b6842efb15545d14941
Anthony D'Ambrosio <anthony.dayzerostudio@gmail.com>
2018-06-28 17:17:59 +0200

    added checkWifiTODL, checks for ~/TODL_${wifi_name} files to cat when using that wifi

--- zsh/fns/chpwd.zsh
+++ zsh/fns/chpwd.zsh
@@ -3,16 +3,15 @@ function cache {
     case "$1" in
         clear|purge) [ -d "$cache_root" ] && command rm -r "$cache_root" ;;
         *)
-            local stamp_file="$cache_root/`pwd`/cache.$1.date"
-            local interval="$2"
+            local lock_file="$cache_root/`pwd`/$2.lock"
             local now=$(date +%s 2>/dev/null)
-            local last=$(cat $stamp_file 2>/dev/null || echo '0')
+            local last=$(cat $lock_file 2>/dev/null || echo '0')
             local SEC_TO_MIN=60
             local delta=$(($now-$last))
-            interval=$(($interval*$SEC_TO_MIN))
+            local interval=$(($1*$SEC_TO_MIN))
             if [ $delta -ge $interval ]; then
-                eval "$(echo ${@:3} | tr ' ' ';')"
-                mkdir -p "$(dirname $stamp_file)" && echo "$now" > $stamp_file
+                (eval "${@:2}" &)
+                mkdir -p "$(dirname $lock_file)" && echo "$now" > "$lock_file"
             fi
             ;;
     esac
@@ -27,6 +26,16 @@ function listVims {
         echo "&> VIMS:" && echo "$VIMS"
 }
 
+function checkWifiTODL {
+    local wifi_name="$(networksetup -getairportnetwork en1 | awk -F':' '{print $2}')"
+    if [[ -e "~/TODL_$wifi_name" ]]; then
+        echo "&> TODL:"
+        cat "~/TODL_$wifi_name"
+    fi
+}
+
 function chpwd {
-    cache chpwd 15 showTodos listVims
+    cache 15 showTodos
+    cache 5 listVims
+    cache 5 checkWifiTODL
 }
