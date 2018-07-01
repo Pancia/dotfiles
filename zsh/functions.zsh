a184e3b2ffb17da685d9482fce153573de10542f
Anthony D'Ambrosio <anthony.dayzerostudio@gmail.com>
2018-07-01 17:49:24 +0200

    adding basic watch function

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -5,6 +5,23 @@ function wait-for {
     "$@"
 }
 
+function watch {
+    local cmd_file="$1"
+    local args="${@:2}"
+    local watch=".$(basename $cmd_file).watch"
+    echo `stat -f%m $cmd_file` > $watch
+    while true; do
+        local mtime=`stat -f%m $cmd_file`
+        if [ `cat $watch 2> /dev/null` != $mtime ]; then
+            echo '======================'
+            eval $cmd_file $args
+            echo '======================'
+            echo $mtime > $watch
+        fi
+        sleep 1
+    done
+}
+
 function cat { command tail -n +1 "$@" }
 function less { command less -N "$@" }
 function ls { command ls -h "$@" }
