8dec030beb889990273012daeacc3a641b7fa697
Anthony D'Ambrosio <anthony.dayzerostudio@gmail.com>
2018-07-01 15:34:16 +0200

    adding .zshrc .vimrc to source resp files moved fns sourcing to end of functions so we can have global fns (might work this way?)

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -1,8 +1,3 @@
-local functions_dir=`dirname $0`/fns
-for f in $(ls $functions_dir); do
-    source $functions_dir/$f
-done
-
 function wait-for {
     echo "[wait-for]: Waiting for \`test -e $1\`, will execute \`${@:2}\`"
     local i=0; while [ ! -e "$1" ]; do; sleep 1; ((i++)); echo -n "\rWaited: $i seconds"; done;
@@ -18,4 +13,11 @@ function reset { tput reset }
 function vim { TERM_TYPE=nvim nvim "$@" }
 function vimrc { vim ~/dotfiles/nvim/init.vim }
 function zshrc { vim ~/dotfiles/zsh/zshrc }
+function .zshrc { source ~/dotfiles/zsh/zshrc }
+function .vimrc { source ~/dotfiles/nvim/init.vim }
 function dot { vim ~/dotfiles }
+
+local functions_dir=`dirname $0`/fns
+for f in $(ls $functions_dir); do
+    source $functions_dir/$f
+done
