d9604117fc0925ee6614459b1ea18aac21544c81
Anthony D'Ambrosio <anthony.dayzerostudio@gmail.com>
2018-07-02 23:16:34 +0200

    added zsh plugins, auto-completion and q (for vim registers in the shell!)

--- zsh/antigen.zsh
+++ zsh/antigen.zsh
@@ -2,7 +2,7 @@ source ~/dotfiles/antigen/antigen.zsh
 
 antigen use oh-my-zsh
 
-# Bundles from the default repo (robbyrussell's oh-my-zsh).
+# SHELL & CO.
 antigen bundle fasd
 antigen bundle git
 antigen bundle git-flow
@@ -16,11 +16,13 @@ antigen bundle brew
 antigen bundle brew-cask
 
 # ZSH
+antigen bundle zsh-users/zsh-autosuggestions
+antigen bundle popstas/zsh-command-time
+antigen bundle cal2195/q
 antigen bundle zsh-users/zsh-syntax-highlighting
 antigen bundle zsh-users/zsh-history-substring-search
 
-# Load the theme.
+# THEME
 antigen theme bhilburn/powerlevel9k powerlevel9k.zsh-theme
 
-# Tell antigen that you're done.
 antigen apply
