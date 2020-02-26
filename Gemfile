ad82388736240b29523cd78a8dcb8d09786afdab
Anthony D'Ambrosio <anthony.dayzerostudio@gmail.com>
2020-02-25 17:39:08 -0800

    fixing ruby irb history

--- Gemfile
+++ Gemfile
@@ -1,4 +1,5 @@
 source "https://rubygems.org"
 
 gem "json", "2.1.0"
+gem "irb"
 gem "rb-readline", "0.5.5"
--- zsh/env/ruby.zsh
+++ zsh/env/ruby.zsh
@@ -1,1 +1,2 @@
 export RUBYLIB=/Users/$(whoami)/dotfiles/lib/ruby
+export PATH="/usr/local/opt/ruby/bin:$PATH"
