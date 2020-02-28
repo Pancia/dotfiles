1d2fc7ba9fe24f40d4b09b979b12683b0f744a2c
Anthony D'Ambrosio <anthony.dayzerostudio@gmail.com>
2020-02-28 15:53:27 -0800

    lotus: clarifying menu title

--- spoons/Lotus/init.lua
+++ spoons/Lotus/init.lua
@@ -53,7 +53,7 @@ function renderMenuBar(text)
     local sound = obj.sounds[obj._soundIdx]
     obj._menubar:setIcon(obj.spoonPath.."/lotus-flower.png")
     local soundTitle = sound.name or sound.path:match("[^/]+$"):match("[^.]+")
-    local title = text or obj._timerCounter .. "#" .. soundTitle
+    local title = text or obj._timerCounter .. "->" .. soundTitle
     obj._menubar:setTitle(title)
 end
 
