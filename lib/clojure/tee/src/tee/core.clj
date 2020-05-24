80d433860c9f968cca406cbcc3a460b4432ba893
Anthony D'Ambrosio <anthony.dayzerostudio@gmail.com>
2020-05-23 20:06:08 -0700

    clojure: using interactive nrepl, adding my tee nrepl middleware for also printing to the terminal when a user command is eval'd

---
+++ lib/clojure/tee/src/tee/core.clj
@@ -0,0 +1,32 @@
+(ns tee.core
+  (:require
+    [nrepl.middleware :refer [set-descriptor!]])
+  (:import
+    [nrepl.transport Transport]))
+
+(defn is-user-eval? [{:keys [op file]}]
+  (and (= op "eval") file))
+
+(defn tee-transport [{:as msg :keys [transport]}]
+  (assoc msg :transport
+    (reify Transport
+      (recv [_] (.recv transport))
+      (recv [_ timeout] (.recv transport timeout))
+      (send [this response]
+        (when (is-user-eval? msg)
+          (when-let [out (:out response)]
+            (.println System/out out))
+          (when-let [err (:err response)]
+            (.println System/out err)))
+        (.send transport response)
+        this))))
+
+(defn middleware [h]
+  (fn [msg]
+    (when (is-user-eval? msg)
+      (.println System/out (:code msg)))
+    (h (tee-transport msg))))
+
+(set-descriptor! #'middleware
+  ;; expects: below / after
+  {:expects #{"eval"}})
