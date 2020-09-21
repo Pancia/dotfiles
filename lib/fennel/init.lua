05fe92347a8533953eb219497bff88d0215c9a5a
Anthony D'Ambrosio <anthony.dayzerostudio@gmail.com>
2020-09-20 19:10:55 -0700

    hs: fixing require of luarocks fennel

--- lib/fennel/init.lua
+++ lib/fennel/init.lua
@@ -158,8 +158,9 @@ localInstall("Watch", {
     },
 })
 
-local LIB = "/Users/pancia/dotfiles/lib/fennel/"
+package.path = '/usr/local/Cellar/luarocks/3.3.1/share/lua/5.3/?.lua;'..package.path
 fennel = require("fennel")
-fennel.path = LIB.."/?.fnl;"..LIB.."/spacehammer/?.fnl;".. fennel.path
+local LIB = "/Users/pancia/dotfiles/lib/fennel/"
+fennel.path = LIB.."/?.fnl;"..LIB.."/spacehammer/?.fnl;"..fennel.path
 table.insert(package.loaders or package.searchers, fennel.searcher)
 require("core")
