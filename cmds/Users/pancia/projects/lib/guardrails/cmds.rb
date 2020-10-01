52338c72aefe006ceaa287a1568ba5f4b16d3929
Anthony D'Ambrosio <anthony.dayzerostudio@gmail.com>
2020-10-01 10:02:04 -0700

    cmds: init guardrails

---
+++ cmds/Users/pancia/projects/lib/guardrails/cmds.rb
@@ -0,0 +1,28 @@
+module CMD
+  def test(opts)
+    opts.banner = "Usage: test"
+    opts.info = "run the clojure tests"
+    lambda { |*args|
+      EXE.system %{
+        clj \
+          -A:spec-dev:dev:test:clj-tests \
+          -J-Dguardrails.enabled=true \
+          -J-Dguardrails.config=guardrails-test.edn \
+          --watch
+      }
+    }
+  end
+  def repl(opts)
+    opts.banner = "Usage: repl"
+    opts.info = "run the clojure repl"
+    lambda { |*args|
+      EXE.system %{
+        clj -A:dev:test
+      }
+    }
+  end
+end
+
+trap "SIGINT" do
+  exit 130
+end
