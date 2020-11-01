07c0326cb948da58bb1c36a5f9b3eb322b9b1b5e
Anthony D'Ambrosio <anthony.dayzerostudio@gmail.com>
2020-11-01 10:06:39 -0800

    adding hammerspoon utils file, currently has memory dump debugging functions

--- lib/fennel/init.lua
+++ lib/fennel/init.lua
@@ -2,6 +2,7 @@
 package.path = '/usr/local/Cellar/luarocks/3.3.1/share/lua/5.3/?.lua;'..package.path
 fennel = require("fennel")
 local LIB = "/Users/pancia/dotfiles/lib/fennel/"
-fennel.path = LIB.."/?.fnl;"..LIB.."/seeds/?.fnl;"..LIB.."/spacehammer/?.fnl;"..fennel.path
+fennel.path = LIB.."/?.fnl;"..LIB.."/lib/?.fnl;"..LIB.."/seeds/?.fnl;"..LIB.."/spacehammer/?.fnl;"..fennel.path
 table.insert(package.loaders or package.searchers, fennel.searcher)
 require("core")
+_ = require("lib/utils")
