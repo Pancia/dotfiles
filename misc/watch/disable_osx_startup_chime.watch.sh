8eb3e987958bb99add662b8af2e3ed178397e36b
Anthony D'Ambrosio <anthony.dayzerostudio@gmail.com>
2021-02-04 18:37:50 -0800

    fixing disable osx startup chime, adding manual install steps as a task in `install` script

--- misc/watch/disable_osx_startup_chime.watch.sh
+++ misc/watch/disable_osx_startup_chime.watch.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env zsh
 
+function sysAudVol() {
+    nvram SystemAudioVolume | cut -d$'\t' -f2
+}
+
 function disable_osx_startup_chime() {
-    local sysAudVol=$(nvram SystemAudioVolume | cut -d$'\t' -f2)
-    if [[ ! "$sysAudVol" == '%80' ]]; then
-        local sysAudVol_binary="$(echo ${sysAudVol} | xxd)"
-        echo "$(date '+%x %X') -> $sysAudVol_binary"
-        #TODO: add to sudoers on install ? with args?
-        #   $(whoami) ALL=(ALL) NOPASSWD: /usr/sbin/nvram SystemAudioVolume=%80
-        echo "SystemAudioVolume: '$sysAudVol_binary'"
+    echo "$(date '+%x %X')"
+    echo "SystemAudioVolume is: '$(sysAudVol | xxd)'"
+    if [[ ! "$(sysAudVol)" == '%80' ]]; then
         sudo nvram SystemAudioVolume=%80
-        echo "SystemAudioVolume = '$(nvram SystemAudioVolume | cut -d$'\t' -f2 | xxd)'"
+        echo "SystemAudioVolume set to: '$(sysAudVol | xxd)'"
     fi
+    echo
 }
 
 disable_osx_startup_chime 2>&1
