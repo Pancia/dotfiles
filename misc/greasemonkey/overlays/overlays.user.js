7aea2c63b3b8a90fb9c39b3af035a8952194ada7
Anthony D'Ambrosio <anthony.dayzerostudio@gmail.com>
2021-04-28 13:09:16 -0700

    fixing greasemonkey overlays script

--- misc/greasemonkey/overlays/overlays.user.js
+++ misc/greasemonkey/overlays/overlays.user.js
@@ -1,95 +1,97 @@
 // ==UserScript==
 // @name     Overlays
-// @version  7
+// @version  9
 // @require  http://code.jquery.com/jquery-latest.min.js
 // @require  https://raw.githubusercontent.com/santhony7/pressAndHold/master/jquery.pressAndHold.js
 // @grant    none
 // ==/UserScript==
 
-function addOverlay(selector, text) {
-    var $overlay = $(`<div id='${selector}'></div>`)
-        .width($(selector).width())
-        .css({
-            'opacity' : 1,
-            'position': 'absolute',
-            'background-color': 'black',
-            'height': '100%',
-            'z-index': 2200
-        });
-    $('<button>', {
-        'text': text || `Show: ${selector}`
-    }).css({
-        'height': '100px',
-        'width': '100%',
-        'font-size': '22px',
-        'background-color': 'white'
-    }).pressAndHold({holdTime: 3000}).on("complete.pressAndHold", () => {
-        $(`[id='${selector}']`).remove();
-    }).appendTo($overlay);
+(function($) {
+    function addOverlay(selector, text) {
+        var $overlay = $(`<div id='${selector}'></div>`)
+            .width($(selector).width())
+            .css({
+                'opacity' : 1,
+                'position': 'absolute',
+                'background-color': 'black',
+                'height': '100%',
+                'z-index': 2200
+            });
+        $('<button>', {
+            'text': text || `Show: ${selector}`
+        }).css({
+            'height': '100px',
+            'width': '100%',
+            'font-size': '22px',
+            'background-color': 'white'
+        }).pressAndHold({holdTime: 3000}).on("complete.pressAndHold", () => {
+            $(`[id='${selector}']`).remove();
+        }).appendTo($overlay);
 
-    $overlay.prependTo(selector)
-}
+        $overlay.prependTo(selector)
+    }
 
-function waitForElementToBeLoaded(selector, callback) {
-    var tid = setInterval(function() {
-        // wait for x element to have been loaded, ie: not null
-        var x = $(selector)[0]
-        if (x == null) { return }
-        // x was loaded, go ahead!
-        clearInterval(tid)
-        callback(x)
-    }, 100);
-}
+    function waitForElementToBeLoaded(selector, callback) {
+        var tid = setInterval(function() {
+            // wait for x element to have been loaded, ie: not null
+            var x = $(selector)[0]
+            if (x == null) { return }
+            // x was loaded, go ahead!
+            clearInterval(tid)
+            callback(x)
+        }, 100);
+    }
 
-function MAIN() {
-    if (window.location.host == "www.youtube.com") {
-        waitForElementToBeLoaded("#comments #comment", () => {
-            addOverlay("#comments", "STOP!, is it really worth it?");
-        });
-        waitForElementToBeLoaded("#related #dismissable", () => {
-            addOverlay("#related");
-        });
-        waitForElementToBeLoaded("#primary", () => {
-            addOverlay("#primary", "Remember you shouldn't be watching yt/anime on the iMac!");
-        });
-        waitForElementToBeLoaded("#avatar-btn", () => {
-            $("#avatar-btn").click()
-            $("ytd-popup-container").hide()
-            waitForElementToBeLoaded("#channel-container #email", (x) => {
-                if (x.title == "anthony@itausa.net") {
-                    $("[id='#primary']").remove()
-                }
-                $("ytd-popup-container").show()
+    function MAIN() {
+        if (window.location.host == "www.youtube.com") {
+            waitForElementToBeLoaded("#comments #comment", () => {
+                addOverlay("#comments", "STOP!, is it really worth it?");
+            });
+            waitForElementToBeLoaded("#related #dismissable", () => {
+                addOverlay("#related");
+            });
+            waitForElementToBeLoaded("#primary", () => {
+                addOverlay("#primary", "Remember you shouldn't be watching yt/anime on the iMac!");
+            });
+            waitForElementToBeLoaded("#avatar-btn", () => {
                 $("#avatar-btn").click()
+                $("ytd-popup-container").hide()
+                waitForElementToBeLoaded("#channel-container #email", (x) => {
+                    if (x.title == "anthony@itausa.net") {
+                        $("[id='#primary']").remove()
+                    }
+                    $("ytd-popup-container").show()
+                    $("#avatar-btn").click()
+                });
             });
-        });
+        }
+
+        if (window.location.host == "www.linkedin.com") {
+            waitForElementToBeLoaded(".news-module", () => {
+                addOverlay(".news-module");
+            });
+            if (window.location.pathname == "/feed/") {
+                waitForElementToBeLoaded("#main", () => {
+                    addOverlay("#main");
+                });
+            }
+            if (window.location.pathname == "/notifications/") {
+                waitForElementToBeLoaded("#main", () => {
+                    addOverlay("#main");
+                });
+            }
+            if (window.location.pathname.startsWith("/news/daily-rundown/")) {
+                waitForElementToBeLoaded("#main", () => {
+                    addOverlay("#main");
+                });
+            }
+        }
     }
 
-    if (window.location.host == "www.linkedin.com") {
-        waitForElementToBeLoaded(".news-module", () => {
-            addOverlay(".news-module");
-        });
-        if (window.location.pathname == "/feed/") {
-            waitForElementToBeLoaded(".core-rail", () => {
-                addOverlay(".core-rail");
-            });
+    var pageURLCheckTimer = setInterval(function() {
+        if (this.lastUrl !== location.href || ! this.lastUrl) {
+            this.lastUrl = location.href;
+            MAIN();
         }
-        if (window.location.pathname == "/notifications/") {
-            waitForElementToBeLoaded(".core-rail", () => {
-                addOverlay(".core-rail");
-            });
-        }
-        if (window.location.pathname.startsWith("/news/daily-rundown/")) {
-            waitForElementToBeLoaded(".core-rail", () => {
-                addOverlay(".core-rail");
-            });
-        }
-    }
-}
-
-var pageURLCheckTimer = setInterval(function() {
-    if (this.lastUrl !== location.href || ! this.lastUrl) {
-        this.lastUrl = location.href;
-        MAIN();
-    }
-}, 222);
+    }, 222);
+})(jQuery);
