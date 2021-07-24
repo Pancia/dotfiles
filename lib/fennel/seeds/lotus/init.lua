581bcde982820db430a32003e16b07b8dea83a55
Anthony D'Ambrosio <anthony.dayzerostudio@gmail.com>
2021-07-24 14:11:02 +0200

    upgrading lotus: using state machine logic, adding system sleep watcher, simplifying pause to just add time to the current time left

--- lib/fennel/seeds/lotus/init.lua
+++ lib/fennel/seeds/lotus/init.lua
@@ -6,13 +6,25 @@ obj.spoonPath = os.getenv("HOME").."/dotfiles/lib/fennel/seeds/lotus/"
 
 obj.interval = { minutes = 30 }
 
+function timeConfig(x)
+    local i = x.interval
+    if i.minutes then
+        return { interval = i.minutes, refreshRate = 60 }
+    else
+        return { interval = i.seconds, refreshRate = 1 }
+    end
+end
+
 function obj:playAwarenessSound()
-    local sound = obj.sounds[obj._soundIdx]
-    local volume = sound.volume or 1
-    local s = sound.name
+    if obj._numTimesSoundPlayed < 3 then
+        obj._numTimesSoundPlayed = obj._numTimesSoundPlayed + 1
+        local sound = obj.sounds[obj._soundIdx]
+        local volume = sound.volume or 1
+        local s = sound.name
         and hs.sound.getByName(sound.name)
         or hs.sound.getByFile(obj.spoonPath.."/"..sound.path)
-    obj.lastPlayedSound = s:volume(volume):play()
+        obj.lastPlayedSound = s:volume(volume):play()
+    end
     return self
 end
 
@@ -24,83 +36,54 @@ function obj:stopAwarenessSound()
     return self
 end
 
-function renderMenuBar()
-    local title
-    local _, refreshRate = userIntervalToSeconds(obj.interval)
-    if obj._paused then
-        if obj._pauseTimer then
-            local nextTrigger = obj._pauseTimer:nextTrigger()
-            title = "|| " .. math.max(math.ceil(nextTrigger / refreshRate), 0)
-        else
-            title = "||"
-        end
-    else
-        local sound = obj.sounds[obj._soundIdx]
-        obj._menubar:setIcon(obj.spoonPath.."/lotus-flower.png")
-        local soundTitle = sound.name or sound.path:match("[^/]+$"):match("[^.]+")
-        local nextTrigger = obj._lotusTimer:nextTrigger()
-        title = math.max(math.ceil(nextTrigger / refreshRate), 0)
-            .. "->[" .. obj._soundIdx .. "/" .. #obj.sounds .."]#" .. soundTitle
-    end
+function obj:renderMenuBar()
+    local sound = obj.sounds[obj._soundIdx]
+    obj._menubar:setIcon(obj.spoonPath.."/lotus-flower.png")
+    local soundTitle = sound.name or sound.path:match("[^/]+$"):match("[^.]+")
+    local timeLeft = math.ceil(obj._timeLeft)
+    local title = timeLeft .. "->[" .. obj._soundIdx .. "/" .. #obj.sounds .."]#" .. soundTitle
     obj._menubar:setTitle(title)
 end
 
 function renderMenu()
-    if obj._paused then
-        return {
-            {title = "resume"
-            , fn = function()
-                obj._paused = false
-                obj._lotusTimer:fire()
-                if obj._pauseTimer then
-                    obj._pauseTimer:stop()
-                    obj._pauseTimer = nil
+    return {
+        {title = "add time"
+        , fn = function()
+            obj.stopAwarenessSound()
+            durp:show({
+                onDuration = function(duration)
+                    obj._state = "countdown"
+                    obj._timeLeft = obj._timeLeft + duration
+                    obj:renderMenuBar()
                 end
-                obj.stopAwarenessSound()
-                obj._lotusTimer = obj._lotusTimer:setNextTrigger(obj._lastNextTrigger or 0)
-                obj._menuRefreshTimer:fire()
-            end},
-            {title = "restart"
-            , fn = function()
-                obj._paused = false
-                obj._soundIdx = 1
-                obj._lotusTimer:fire()
-                if obj._pauseTimer then
-                    obj._pauseTimer:stop()
-                    obj._pauseTimer = nil
-                end
-                obj.stopAwarenessSound()
-                obj._lotusTimer = obj._lotusTimer:start()
-                obj._menuRefreshTimer:fire()
-            end},
-        }
+            })
+        end},
+        {title = "restart"
+        , fn = function()
+            obj.stopAwarenessSound()
+            obj._state = "countdown"
+            obj._soundIdx = 1
+            obj._numTimesSoundPlayed = 0
+            obj:renderMenuBar()
+        end},
+    }
+end
+
+function obj:notifCallback()
+    obj.stopAwarenessSound()
+    obj._numTimesSoundPlayed = 0
+    local sound = obj.sounds[obj._soundIdx]
+    local action = sound.action
+    local resume = function()
+        obj._state = "countdown"
+        obj._timeLeft = timeConfig(obj).interval
+        obj._soundIdx = (obj._soundIdx % #obj.sounds) + 1
+        obj:renderMenuBar()
+    end
+    if action then
+        action(resume)
     else
-        return {
-            {title = "pause for ...?"
-            , fn = function()
-                obj.stopAwarenessSound()
-                durp:show({
-                    onDuration = function(duration)
-                        obj._paused = true
-                        if obj._pauseTimer then
-                            obj._pauseTimer:stop()
-                            obj._pauseTimer = nil
-                        end
-                        obj._menuRefreshTimer:fire()
-                        obj._lastNextTrigger = obj._lotusTimer:nextTrigger()
-                        obj._lotusTimer = obj._lotusTimer:stop()
-                        _, refreshRate = userIntervalToSeconds(obj.interval)
-                        obj._pauseTimer = hs.timer.doAfter(refreshRate*duration, function()
-                            obj._pauseTimer = nil
-                            obj._paused = false
-                            obj._menuRefreshTimer:fire()
-                            obj._lotusTimer = obj._lotusTimer:setNextTrigger(obj._lastNextTrigger)
-                        end)
-                        obj._menuRefreshTimer:fire()
-                    end
-                })
-            end},
-        }
+        resume()
     end
 end
 
@@ -108,63 +91,10 @@ function with_default(obj, key, default)
     obj[key] = obj[key] or default
 end
 
-function snoozeTimer()
-    durp:show({
-        onDuration = function(duration)
-            obj._paused = true
-            obj.stopAwarenessSound()
-            obj._lotusTimer = obj._lotusTimer:stop()
-            _, refreshRate = userIntervalToSeconds(obj.interval)
-            obj._pauseTimer = hs.timer.doAfter(refreshRate*duration, function()
-                obj._pauseTimer = nil
-                obj._paused = false
-                obj._menuRefreshTimer:fire()
-                obj._lotusTimer = obj._lotusTimer:setNextTrigger(0)
-            end)
-            obj._menuRefreshTimer:fire()
-        end,
-        onClose = function()
-            obj._paused = false
-            obj.stopAwarenessSound()
-            obj._lotusTimer = obj._lotusTimer:start()
-            obj._menuRefreshTimer:fire()
-        end
-    })
-end
-
-function obj:notifCallback()
-    if obj._soundRepeat then
-      obj._soundRepeat:stop()
-      obj._soundRepeat = nil
-    end
-    local action = obj._sound.action
-    local activationType = obj._notif:activationType()
-    if activationType == hs.notify.activationTypes.actionButtonClicked then
-        snoozeTimer()
-    else
-        local resume = function()
-            obj._paused = false
-            obj._soundIdx = (obj._soundIdx % #obj.sounds) + 1
-            obj.stopAwarenessSound()
-            obj._lotusTimer = obj._lotusTimer:start()
-            obj._menuRefreshTimer:fire()
-        end
-        if action then
-            action(resume)
-        else
-            resume()
-        end
-    end
-end
-
-function lotusBlock()
-    obj._sound = obj.sounds[obj._soundIdx]
-    obj:playAwarenessSound()
-    if obj._sound.notif then
-        obj._lotusTimer = obj._lotusTimer:stop()
-        local notification = obj._sound.notif()
-        with_default(notification, "hasActionButton", true)
-        with_default(notification, "actionButtonTitle", "SNOOZE")
+function obj:notify()
+    local sound = obj.sounds[obj._soundIdx]
+    if sound.notif then
+        local notification = sound.notif()
         obj._notif = hs.notify.new(obj.notifCallback, notification):send()
         clearCheck = hs.timer.doEvery(1, function()
             if not hs.fnutils.contains(hs.notify.deliveredNotifications(), obj._notif) then
@@ -178,84 +108,94 @@ function lotusBlock()
                 obj._notif = nil
             end
         end)
-        obj._soundRepeat = hs.timer.doEvery(60, function()
-          obj:playAwarenessSound()
-        end)
     end
 end
 
-function userIntervalToSeconds(x)
-    if x.minutes then
-        return x.minutes * 60, 60
+function obj:ensureNotifDelivered()
+    obj._logger.df("[ensureNotifDelivered] state = %s", obj._state)
+    if obj._state == "notif" and not obj._notif then
+        obj:notify()
+    end
+    if obj._state ~= "sleeping" then
+        obj:playAwarenessSound()
+    end
+end
+
+function obj:heartbeat()
+    obj._logger.df("HeartBeat state = %s, ", obj._state)
+    if obj._state == "countdown" then
+        obj._timeLeft = obj._timeLeft - 1
+        if obj._timeLeft <= 0 then
+            obj._state = "notif"
+            obj:notify()
+            obj:playAwarenessSound()
+        end
+    elseif obj._state == "notif" then
+        obj:ensureNotifDelivered()
+    elseif obj._state == "sleeping" then
     else
-        return x.seconds, 1
+        obj._logger.ef("UNEXPECTED STATE: %s", hs.inspect(obj._state))
+    end
+    obj:renderMenuBar()
+end
+
+function watchSystem(eventType)
+    if eventType == hs.caffeinate.watcher.systemDidWake then
+        obj._logger.df("systemDidWake")
+        obj._state = obj._prevState
+        obj._prevState = nil
+    elseif eventType == hs.caffeinate.watcher.systemWillSleep then
+        obj._logger.df("systemWillSleep")
+        obj._prevState = obj._state
+        obj._state = "sleeping"
     end
 end
 
 function obj:start(config)
+    obj._logger = hs.logger.new("Lotus", "debug")
+
     for k,v in pairs(config) do obj[k] = v end
     local saved = {
+        ["state"] = hs.settings.get("state"),
         ["soundIdx"] = hs.settings.get("soundIdx"),
-        ["pausedFor"] = hs.settings.get("pausedFor"),
-        ["nextTrigger"] = hs.settings.get("nextTrigger"),
+        ["timeLeft"] = hs.settings.get("timeLeft"),
     }
-    hs.printf("Lotus started with: %s", hs.inspect(saved))
+
+    obj._logger.f("Started with: %s", hs.inspect(saved))
+
+    obj._state = saved["state"] or "countdown"
+    obj._numTimesSoundPlayed = 0
     obj._soundIdx = saved["soundIdx"] or #obj.sounds
-    obj._menubar = hs.menubar.new():setMenu(renderMenu)
-
-    interval, refreshRate = userIntervalToSeconds(obj.interval)
-    obj._lotusTimer = hs.timer.doEvery(interval, lotusBlock)
-
-    if saved["pausedFor"] then
-        obj._paused = true
-        obj._lotusTimer = obj._lotusTimer:stop()
-        obj._lastNextTrigger = saved["nextTrigger"]
-        obj._pauseTimer = hs.timer.doAfter(saved["pausedFor"], function()
-            obj._pauseTimer = nil
-            obj._paused = false
-            obj._lotusTimer = hs.timer.doEvery(interval, lotusBlock)
-            obj._lotusTimer = obj._lotusTimer:setNextTrigger(obj._lastNextTrigger)
-            obj._menuRefreshTimer:fire()
-        end)
-    else
-        obj._lotusTimer:setNextTrigger(((saved["nextTrigger"] or interval+1) < interval) and saved["nextTrigger"] or interval)
-    end
+    obj._timeLeft = saved["timeLeft"] or timeConfig(obj).interval
 
     if not saved["soundIdx"] then
         obj:playAwarenessSound()
     end
 
-    renderMenuBar()
-    obj._menuRefreshTimer = hs.timer.doEvery(refreshRate, renderMenuBar)
+    obj._menubar = hs.menubar.new():setMenu(renderMenu)
+    obj._heartbeat = hs.timer.doEvery(timeConfig(obj).refreshRate, obj.heartbeat)
+    obj._systemWatcher = hs.caffeinate.watcher.new(watchSystem):start()
+
+    obj:renderMenuBar()
+
+    obj._logger.f("obj = %s", hs.inspect(obj))
 
     return self
 end
 
 function saveState()
+    hs.settings.set("state", obj._state)
     hs.settings.set("soundIdx", obj._soundIdx)
-    if obj._paused then
-        hs.settings.set("pausedFor", obj._pauseTimer:nextTrigger())
-        hs.settings.set("nextTrigger", obj._lastNextTrigger)
-    else
-        hs.settings.set("pausedFor", nil)
-        hs.settings.set("nextTrigger", obj._lotusTimer:nextTrigger())
-    end
+    hs.settings.set("timeLeft", obj._timeLeft)
 end
 
 function obj:stop()
     saveState()
-    if obj._pauseTimer then
-        obj._pauseTimer:stop()
-        obj._pauseTimer = nil
-    end
     if obj._notif then
         obj._notif:withdraw()
     end
-    if obj._lotusTimer then
-        obj._lotusTimer:stop()
-    end
-    if obj._menuRefreshTimer then
-      obj._menuRefreshTimer:stop()
+    if obj._heartbeat then
+        obj._heartbeat:stop()
     end
     if obj._menubar then
       obj._menubar:delete()
