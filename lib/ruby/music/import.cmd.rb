c16ebd9aac7edbb629045d607899c942873f6cd9
Anthony D'Ambrosio <anthony.dayzerostudio@gmail.com>
2021-09-17 16:22:06 +0200

    adding artist option to music import
    fixing music dir location

--- lib/ruby/music/import.cmd.rb
+++ lib/ruby/music/import.cmd.rb
@@ -8,32 +8,40 @@ module MusicCMD
     opts.on("-p", "--playlist PLAYLIST_NAME", "String to use as FILES's PLAYLIST metadata") { |pl|
       $options[:playlist] = pl
     }
+    opts.on("-a", "--artist ARTIST", "String to use as FILES's ARTIST metadata") { |pl|
+      $options[:artist] = pl
+    }
     lambda { |*files|
       num_files = files.count
       files.each_with_index { |f, idx|
         puts ">>> ##{idx+1}/#{num_files} => #{f}".reverse
         sanitized = %x[basename #{Shellwords.escape f}].gsub(/[^\w\-\#\.\(\)\&\[\]\,\;\'\" ]/, "")
-        playlist = $options[:playlist] || %x[dirname #{Shellwords.escape f}].gsub(/\_/, " ").strip
         yt_id = sanitized[/(.*)__#__([^\.]+)\.m4a/, 2]
         title = sanitized[/(.*)__#__([^\.]+)\.m4a/, 1]
-        RbReadline.prefill_prompt title
-        artist = Readline.readline("artist>?:".reverse).chomp
+        if $options[:artist] then
+          artist = $options[:artist]
+        else
+          RbReadline.prefill_prompt title
+          artist = Readline.readline("artist>?:".reverse).chomp
+        end
         RbReadline.prefill_prompt title
         name = Readline.readline("name>?:".reverse).chomp
         uuid = %x[uuidgen].strip
         song = {:from => :ytdl,
                 :id => uuid,
-                :playlist => playlist,
+                :playlist => $options[:playlist],
                 :url => yt_id,
                 :artist => artist,
                 :name => name}
-        p song if $options[:dry_run] or $options[:verbose]
-        MusicDB.append song if not $options[:dry_run]
-        execute("mv #{Shellwords.escape f} $MUSIC_DIR/#{uuid}.m4a")
-        MusicDB.tag([{"id" => uuid,
-                      "artist" => artist,
-                      "name" => name,
-                      "playlist" => playlist}])
+        p song if $options[:verbose]
+        if not $options[:dry_run] then
+          MusicDB.append song
+          execute("mv #{Shellwords.escape f} $MUSIC_DIR/#{uuid}.m4a")
+          MusicDB.tag([{"id" => uuid,
+                        "artist" => artist,
+                        "name" => name,
+                        "playlist" => $options[:playlist]}])
+        end
       }
     }
   end
