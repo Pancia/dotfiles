b8afcc262594ba4ed21b6505e86a4df1ba011d95
Anthony D'Ambrosio <anthony.dayzerostudio@gmail.com>
2021-10-13 18:14:55 +0200

    music filter: can now filter by tags

--- lib/ruby/music/filter.cmd.rb
+++ lib/ruby/music/filter.cmd.rb
@@ -1,21 +1,29 @@
 require 'music/music_db.rb'
+require 'set'
 
 module MusicCMD
 
   def filter(opts)
     opts.banner = "Usage: filter [OPTS]"
     opts.info = "Print all matching songs according to FILTER"
-    opts.on("-f", "--filter FILTER", "A comma separated list of playlists") { |f|
-      $options[:filter] = f
-    }
+    opts.on("-p", "--by-playlist PLAYLISTS", "A comma separated list of playlists") { |f| $options[:by_playlist] = f }
+    opts.on("-t", "--by-tags TAGS", "A comma separated list of tags") { |f| $options[:by_tags] = f }
     lambda {
-      raise "--filter is required!" if not $options[:filter]
+      raise "a filter is required!" if not $options[:by_playlist] and not $options[:by_tags]
+      raise "only one filter is allowed!" if $options[:by_playlist] and $options[:by_tags]
       mdb = MusicDB.read()
-      pickme = $options[:filter].split(",")
-      by_playlist = mdb.map{|_,x| x}.group_by{|x| x["playlist"]}
-      by_playlist.select{|name, list| pickme.include?(name)}
-        .values.flatten.map{|x| x["id"]}
-        .each{|f| puts "#{f}.m4a"}
+      if $options[:by_tags] then
+        filter = $options[:by_tags].split(",")
+        mdb.map{|_,x| x}.select{|x| not filter.to_set.intersection((x["tags"] or "").split(",").to_set).empty?}
+          .map{|x| x["id"]}
+          .each{|id| puts "#{id}.m4a"}
+      else
+        filter = $options[:by_playlist].split(",")
+        by_playlist = mdb.map{|_,x| x}.group_by{|x| x["playlist"]}
+        by_playlist.select{|name, list| filter.include?(name)}
+          .values.flatten.map{|x| x["id"]}
+          .each{|id| puts "#{id}.m4a"}
+      end
     }
   end
 
