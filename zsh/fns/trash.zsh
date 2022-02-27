7e580c46aef96e69acfbb4cab74aaad0e066d7c0
Anthony D'Ambrosio <anthony.dayzerostudio@gmail.com>
2022-02-27 17:08:54 +0100

    improving readability of restore command

--- zsh/fns/trash.zsh
+++ zsh/fns/trash.zsh
@@ -4,7 +4,7 @@ export TRASH_CACHE_HISTORY="$HOME/.cache/dotfiles/trash/history"
 
 function _record_trash {
     [ ! -d "$TRASH_CACHE_HISTORY" ] && mkdir -p "$(dirname $TRASH_CACHE_HISTORY)"
-    printf '%s\t%s\n' "$f" "$dest" >> "$TRASH_CACHE_HISTORY"
+    printf '%s\t%s\t%s\t%s\n' "$1" "$2" "$3" "$4" >> "$TRASH_CACHE_HISTORY"
     if [[ $TRASH_CACHE_MAX_HISTORY -lt $(wc -l "$TRASH_CACHE_HISTORY" | awk '{print $1}') ]]; then
         local tmp="$(mktemp)"
         awk -F'\t' '{if (NR != 1) { print $0 } }' "$TRASH_CACHE_HISTORY" > "$tmp"
@@ -24,7 +24,7 @@ function trash { # mv to ~/.Trash
             local dest="${HOME}/.Trash/${prefix}>>>${f//\//%}<<<${suffix}"
             echo "[dotfiles/trash] INFO: moving '${f}' to '${dest}'"
             mv "${f}" "${dest}"
-            _record_trash "$f" "$dest"
+            _record_trash "$f" "$prefix" "$suffix" "$dest"
         else
             failed=true
             >&2 echo "[dotfiles/trash] ERROR: file not found: '$f'"
@@ -36,17 +36,17 @@ function trash { # mv to ~/.Trash
 function restore { # restore from ~/.Trash
     case "$1" in
         '') selected=$(mktemp)
-            command cat -n $TRASH_CACHE_HISTORY | awk -F'\t' '{print $1"\t"$3}' | tail -r \
+            command cat -n $TRASH_CACHE_HISTORY | tail -r \
                 | search --on-cancel error > $selected && restore $selected
             ;;
         *)
             line_num="$(cat $1 | cut -f1)"
             [ -z "$line_num" ] && >&2 echo "[ERROR][restore]: Must select a line" && return 1
             local line="$(awk "{ if (NR == $line_num) { print \$0 } }" $TRASH_CACHE_HISTORY)"
-            local file="$(basename `echo $line | cut -f2` | sed -E 's/(>>>)|(<<<)/\\t/g')"
+            local file="$(basename `echo $line | cut -f4` | sed -E 's/(>>>)|(<<<)/\\t/g')"
             local folder="$(echo $file | cut -f1 | sed 's/%/\//g')"
             local fname="$(echo $file | cut -f2 | sed 's/%/\//g')"
-            local src="$(echo $line | cut -f2)"
+            local src="$(echo $line | cut -f4)"
             if [[ "$fname" =~ "^/" ]]; then
                 local dest="$fname"
             else
