750c8f5aefe93c0dc563e7c7e8e1dfaaaee151ed
Anthony D'Ambrosio <anthony.dayzerostudio@gmail.com>
2022-03-27 15:06:32 +0200

    nvim fixing tab erroring because luasnip did not exist, will want to install a snip lib eventually

--- nvim/lua/plugs/hop.lua
+++ nvim/lua/plugs/hop.lua
@@ -2,10 +2,10 @@
 
 require('hop').setup()
 
-vim.api.nvim_set_keymap('n', '<space>w', "<cmd>:HopWordAC<cr>", {})
-vim.api.nvim_set_keymap('n', '<space>b', "<cmd>:HopWordBC<cr>", {})
-vim.api.nvim_set_keymap('n', '<space>j', "<cmd>:HopLineStartAC<cr>", {})
-vim.api.nvim_set_keymap('n', '<space>k', "<cmd>:HopLineStartBC<cr>", {})
+vim.api.nvim_set_keymap('', '<space>w', "<cmd>:HopWordAC<cr>", {})
+vim.api.nvim_set_keymap('', '<space>b', "<cmd>:HopWordBC<cr>", {})
+vim.api.nvim_set_keymap('', '<space>j', "<cmd>:HopLineStartAC<cr>", {})
+vim.api.nvim_set_keymap('', '<space>k', "<cmd>:HopLineStartBC<cr>", {})
 
 vim.api.nvim_set_keymap('n', 'f', "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR })<cr>", {})
 vim.api.nvim_set_keymap('n', 'F', "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR })<cr>", {})
