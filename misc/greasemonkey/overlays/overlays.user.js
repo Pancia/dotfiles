f0abc1cef18172e6e20557a26b6023e74b826254
Anthony D'Ambrosio <anthony.dayzerostudio@gmail.com>
2023-03-22 18:58:06 +0100

    greasemonkey: pausing videos by default (if enabled by alert)

--- misc/greasemonkey/overlays/overlays.user.js
+++ misc/greasemonkey/overlays/overlays.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name     Overlays
-// @version  27
+// @version  28
 // @require  http://code.jquery.com/jquery-latest.min.js
 // @require  https://raw.githubusercontent.com/santhony7/pressAndHold/master/jquery.pressAndHold.js
 // @require  https://cdn.jsdelivr.net/npm/js-cookie@3.0.1/dist/js.cookie.min.js
@@ -104,6 +104,7 @@
                             });
                         var opts = {holdTime: 3000, progressIndicatorColor: "#ff00ff", progressIndicatorOpacity: 0.3};
                         addOverlayTo("#movie_player", "player_overlay", button, opts);
+                        document.querySelector("video").pause();
                     });
                 });
             }
