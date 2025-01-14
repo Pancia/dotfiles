c7560dc48cc3ff3452ccc9e9d3f89eb801ad71db
Anthony D'Ambrosio <anthony.dayzerostudio@gmail.com>
2025-01-14 10:54:05 -0800

    monitor: adding nochange functionality & trying to skip logging if login screen / logged out

--- lib/lua/seeds/monitor.lua
+++ lib/lua/seeds/monitor.lua
@@ -1,20 +1,29 @@
 local obj = {}
 
 local HOME = os.getenv("HOME")
-
 local log_path = HOME .. "/private/logs/monitor/"
-
 local interval = 60
 
 function writeToLog(text)
+    local output
+    if obj._prevLogEntry == text then
+        output = "<NOCHANGE>\n\n"
+        print("TRUE", output)
+    else
+        output = text
+        obj._prevLogEntry = text
+    end
+    output = "now: " .. os.date("%Y_%m_%d__%H:%M:%S") .. "\n" .. output
     local log_file = log_path .. "/" .. os.date("%Y_%m_%d") .. ".log"
-    io.open(log_file, "a"):write(text):close()
+    io.open(log_file, "a"):write(output):close()
 end
 
 function createEntry(window, allwindows)
+    if window:application():title() == "loginwindow" then
+        return ""
+    end
     local text = ""
-    text = text .. "now: " .. os.date("%Y_%m_%d__%H:%M:%S") .. "\n"
-    text = text .. "focused window: " .. window:application():title() .. "; title: " .. window:title().. "\n"
+    text = text .. "focused window, app: " .. window:application():title() .. "; title: " .. window:title().. "\n"
     text = text .. "visible windows:\n" .. table.concat(hs.fnutils.map(allwindows, function(w)
         return "    app: " .. w:application():title() .. "; title: " .. w:title() .. "\n"
     end))
@@ -23,9 +32,9 @@ function createEntry(window, allwindows)
 end
 
 function obj:start(config)
+    writeToLog(createEntry(hs.window.focusedWindow(), hs.window.visibleWindows()))
     obj._loop = hs.timer.doEvery(interval, function()
-        local log_entry = createEntry(hs.window.focusedWindow(), hs.window.visibleWindows())
-        writeToLog(log_entry)
+        writeToLog(createEntry(hs.window.focusedWindow(), hs.window.visibleWindows()))
     end)
     return self
 end
